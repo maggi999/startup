203a2d590c637ae22c8e893f71e3241b3465e6b9
maggi999 <molitor.magnus@gmail.com>
2016-05-13 18:21:23 +0100

    Läuft für wetter.com mit Postleitzahl

--- welcome.php
+++ welcome.php
@@ -1,32 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
 <?php
 //	$ declares a variable
-$url = $_POST["name"];
+$url = "http://www.wetter.com/suche/?q=";
+$city= $_POST["name"]." ".$_POST["email"];
 echo $url;
 
 /*	Initialising variable output on the fly and then calling one of the internal functions 
 to PHP call file_get_contents(), which goes to an url or file and pulls every data out of it */	
-//$output = file_get_contents($url);
-//echo $output;
+
+ $data = file_get_contents($url . urlencode($city));
+//echo $data;
 
 //	Or better
 //	Initialize library CURL as handle this is where this specific instance is initialized to be stored to that we can work with it.
-$handle = curl_init($url);
+//$handle = curl_init($url);
 
 /*	Set first option in CURL to know what is gonna do when executed. Setting an option in the handle created. Option: CURLOPT_REturntransfer return the transfer to a variable (curl_scraped_page, not on the screen). Else would be just on screen. True otherwise nothing happens */
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+//curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 
 //	Run all the stuff and into the variable (All the same as to file_get_contents)
-$data = curl_exec($handle);
-curl_close($handle);
+//$data = curl_exec($handle);
+//curl_close($handle);
 
 // q=site%3Afroogle.com, field (q=query) always before equals sign and value after,
 /* (.+?) match everything starting from the text, opening anchor text here (.+?) closing anchor here. Always use forward /.*/
-$regex = '/<title>(.+?)</title>/';
-preg_match($regex,$data,$match);
+$regex = '/<title>(.+?)<\/title>/';
+
+//	For wetter.de
+//$temperature = '/temperature-max\">(.+?)<\/span>/';
+
+//	For wetter.com
+$temperature = '/tip--trigger ]\">(.+?)C<\/div>/';
+preg_match($temperature,$data,$match);
 var_dump($match);
 echo $match[1];
 
 
-//echo curl_scraped_page;
-
+//echo curl_scraped_page;*/
 ?>
+
+</body>
+</html>
